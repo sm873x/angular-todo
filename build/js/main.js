6627783a3ed5a81e3cb832c5ce857adf4da694e9
Stella Ma <sm873x@gmail.com>
2016-08-07 18:16:17 -0400

    Clear input field after submit. Added clearInput fn to ListController fn

--- build/js/main.js
+++ build/js/main.js
@@ -69,13 +69,20 @@
     ListController.$inject = ['item'];
 
     function ListController(ItemService) {
+        var that = this;
+
+        function clearInput() {
+            that.newItem = {};
+        }
+
         this.itemList = ItemService.getAll();
         console.log('getAll', this.itemList);
 
         this.newItem = {};
         this.addToList = function addToList(item) {
             ItemService.save(item);
-            this.itemList = ItemService.getAll(); 
+            this.itemList = ItemService.getAll();
+            clearInput();
         };
 
         // this.editedItem = {};
