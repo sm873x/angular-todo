e3465741ff8b49f3c6d180f7bbdd3431e13b6eb0
Stella Ma <sm873x@gmail.com>
2016-08-07 18:13:25 -0400

    Debugged why submitting an item wasn't pushing an a new item into the list. Added assigned newest itemList to addToList fn in ListController

--- build/js/main.js
+++ build/js/main.js
@@ -71,20 +71,21 @@
     function ListController(ItemService) {
         this.itemList = ItemService.getAll();
         console.log('getAll', this.itemList);
+
         this.newItem = {};
         this.addToList = function addToList(item) {
             ItemService.save(item);
-
+            this.itemList = ItemService.getAll(); 
         };
 
-        this.editedItem = {};
-        this.editItem = function editItem(editedItem) {
-            ItemService.save(editedItem);
-        };
+        // this.editedItem = {};
+        // this.editItem = function editItem(editedItem) {
+        //     ItemService.save(editedItem);
+        // };
 
-        this.deleteItem = function deleteItem() {
-
-        };
+        // this.deleteItem = function deleteItem() {
+        //
+        // };
 
 
     }
